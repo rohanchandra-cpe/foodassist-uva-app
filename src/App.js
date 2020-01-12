c7a9cc7bcae3882da8429410a1f257899ddd51bb
Rohan Chandra <rc8yw@virginia.edu>
2020-01-12 12:38:30 -0500

    Basic New Member Registration Page Front End

--- src/App.js
+++ src/App.js
@@ -10,9 +10,43 @@ class App extends Component {
 				</div>
 				<form>
 					<div class="form-group">
-						
+						<label for="orgName" class="text-success">Organization Name</label>
+						<div class = "col-xs-3">
+							<input type="text" class="col-xs-3" placeholder="Organization Name"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="contactName" class="text-success">Contact Name</label>
+						<div class = "col-xs-3">
+							<input type="text" class="col-xs-3" placeholder="Enter Name"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="contactEmail" class="text-success">Contact Email</label>
+						<div class = "col-xs-3">
+							<input type="text" class="col-xs-3" placeholder="Enter Email"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="contactPhoneNumber" class="text-success">Contact Phone Number</label>
+						<div class = "col-xs-3">
+							<input type="text" class="col-xs-3" placeholder="Enter Phone Number"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="password" class="text-success">Password</label>
+						<div class = "col-xs-3">
+							<input type="password" class="col-xs-3" placeholder="Enter Password"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="password" class="text-success">Reenter Password</label>
+						<div class = "col-xs-3">
+							<input type="password" class="col-xs-3" placeholder="Reenter Password"/>
+						</div>
 					</div>
 				</form>
+				<button type="button" class="btn btn-outline-primary" type="submit">Register</button>
 			</div>
 		);
 	}
