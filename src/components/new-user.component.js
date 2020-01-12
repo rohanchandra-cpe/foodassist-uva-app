a7ce95a58a425499fe3b8c7b930d6ccdfe9a6a8b
Rohan Chandra <rc8yw@virginia.edu>
2020-01-12 13:14:35 -0500

    made components folder to hold the different pages. Moved previous APp.js contents to a component called new-user.

---
+++ src/components/new-user.component.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+
+export default class NewUser extends Component {
+	constructor(props){
+		super(props);
+
+		
+	render() {
+		return (
+			<div>
+				<div class="jumbotron">
+					<h1 class="display-3 text-center text-primary">New User Registration</h1>
+				</div>
+				<form>
+					<div class="form-group">
+						<label for="orgName" class="text-success">Organization Name</label>
+						<div class = "col-xs-3">
+							<input type="text" class="col-xs-3" placeholder="Organization Name"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="contactName" class="text-success">Contact Name</label>
+						<div class = "col-xs-3">
+							<input type="text" class="col-xs-3" placeholder="Enter Name"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="contactEmail" class="text-success">Contact Email</label>
+						<div class = "col-xs-3">
+							<input type="text" class="col-xs-3" placeholder="Enter Email"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="contactPhoneNumber" class="text-success">Contact Phone Number</label>
+						<div class = "col-xs-3">
+							<input type="text" class="col-xs-3" placeholder="Enter Phone Number"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="password" class="text-success">Password</label>
+						<div class = "col-xs-3">
+							<input type="password" class="col-xs-3" placeholder="Enter Password"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="password" class="text-success">Reenter Password</label>
+						<div class = "col-xs-3">
+							<input type="password" class="col-xs-3" placeholder="Reenter Password"/>
+						</div>
+					</div>
+				</form>
+				<button type="button" class="btn btn-outline-primary" type="submit">Register</button>
+			</div>
+		);
+	}
+}
+
+export default App;
