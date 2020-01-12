d60111836750278c94a348d5663b7bbee21ec87f
Rohan Chandra <rc8yw@virginia.edu>
2020-01-12 17:38:21 -0500

    More progress on new user registration page

--- src/components/new-user.component.js
+++ src/components/new-user.component.js
@@ -6,7 +6,81 @@ export default class NewUser extends Component {
 	constructor(props){
 		super(props);
 
+		this.onChangeOrgName = this.onChangeOrgName.bind(this);
+		this.onChangeContactName = this.onChangeContactName.bind(this);
+		this.onChangeContactEmail = this.onChangeContactEmail.bind(this);
+		this.onChangeContactPhoneNumber = this.onChangeContactPhoneNumber.bind(this);
+		this.onChangeContactPassword = this.onChangeContactPassword.bind(this);
+		this.onChangeContactRePassword = this.onChangeContactRePassword.bind(this);
+		this.state = {
+			org_name: '',
+			contact_name: '',
+			contact_email: '',
+			contact_phone_number: '',
+			contact_password: '',
+			contact_reenter_password: ''
+		}
+	}
+	onChangeOrgName(e) {
+		this.setState({
+			org_name: e.target.value
+		});
+	}
+	onChangeContactName(e) {
+		this.setState({
+			contact_name: e.target.value
+		});
+	}
+	onChangeContactEmail(e) {
+		this.setState({
+			contact_email: e.target.value
+		});
+	}
+	onChangeContactPhoneNumber(e) {
+		this.setState({
+			contact_phone_number: e.target.value
+		});
+	}
+	onChangeContactPassword(e) {
+		this.setState({
+			contact_password: e.target.value
+		});
+	}
+	onChangeContactRePassword(e) {
+		this.setState({
+			contact_reenter_password: e.target.value
+		});
+	}
+	onSubmit(e) { //Here is where I have to check if the passwords are the same
+		e.preventDefault();
+		console.log(`New User Registered`);
+
+		if(!(this.state.contact_password.equals(this.state.contact_reenter_password))){
+			//Error! Make sure they equal. Maybe some nice bootstrap stuff
+			//I could also do a length check
+		}
+		else{
+			const newUser = {
+				org_name = this.state.org_name,
+				contact_name = this.state.contact_name,
+				contact_email = this.state.contact_email,
+				contact_phone_number = this.state.contact_phone_number,
+				contact_password = this.state.contact_password,
+				contact_reenter_password = this.state.contact_reenter_password
+			};
+			//Then we would axios post
+			this.setState = ({
+				org_name: '',
+				contact_name: '',
+				contact_email: '',
+				contact_phone_number: '',
+				contact_password: '',
+				contact_reenter_password: ''
+			})
+		}
 		
+	}
+
 	render() {
 		return (
 			<div>
